4f5aa365e1c07c66f7decc2401ab7a524507e04b
danny <danvandeee@hotmail.com>
2018-08-29 11:52:31 +0200

    eerste setup

---
+++ Kaart.java
@@ -0,0 +1,23 @@
+package kwartet;
+
+public class Kaart {
+
+	String kleur;
+	String nummerofplaatje;
+	
+	public String getNaam () {
+		return (kleur + nummerofplaatje);
+	}
+	
+	public String getKleur () {
+		
+		return kleur;
+	}
+	
+	public String getNummerofplaatje () {
+		return nummerofplaatje;
+		
+	}
+	
+	
+}
---
+++ KwartetSpel.java
@@ -0,0 +1,23 @@
+package kwartet;
+
+public class KwartetSpel {
+
+	public static void main (String [] args) {
+		
+		KwartetSpel kwartet = new KwartetSpel();
+		kwartet.setUp();
+		
+	}
+	
+	
+	public void setUp () {
+		
+		
+		
+		
+		
+		
+	}
+	
+	
+}
